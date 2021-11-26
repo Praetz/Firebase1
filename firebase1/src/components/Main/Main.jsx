1025d8f51de427eb2712dddf54f2b47dda8d8681
Praetz <michaelpraetz7@gmail.com>
2021-11-26 12:50:41 -0700

    Work on button in Main.jsx

--- firebase1/src/components/Main/Main.jsx
+++ firebase1/src/components/Main/Main.jsx
@@ -8,11 +8,9 @@ export default function Main() {
                 <h1>Main</h1>
             </div>
             <div className='item-container'>
-                <a href='https://www.google.com/search?q=religous+pictures&oq=religous+pictures&aqs=chrome..69i57j0i10l4j0i10i395l2j0i10j0i10i395l2.15817j1j7&sourceid=chrome&ie=UTF-8'>
-                    item 1
-                </a>
+                
                 <label htmlFor="Dailyword">
-                    <button type="submit" onClick='../../images/' value='boolean' href='../../images/footOfCross.jpg' >Dailyword</button>
+                    <button type="submit" value='boolean' href='../../images/footOfCross.jpg' >Dailyword</button>
                 </label>
             </div>
         </div>

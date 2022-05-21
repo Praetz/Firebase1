9c9a0953524044a4557c6fe6352f3cfc5f300979
Praetz <michaelpraetz7@gmail.com>
2022-05-20 19:55:54 -0700

    gitlens

--- firebase1/src/App.jsx
+++ firebase1/src/App.jsx
@@ -1,10 +1,8 @@
-
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
 import Header from './components/Header/Header';
 import Dailyword from "./components/Dailyword/Dailyword"
-
 
 function App() {
   return (
